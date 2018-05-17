299dd4968f26ff4687a07f139d0f62bc01b9cf0b
samjkirk <33060618+samjkirk@users.noreply.github.com>
2018-05-17 11:30:20 +0100

    Version 1

---
+++ src/ParametersAndOperators.java
@@ -0,0 +1,6 @@
+public class ParametersAndOperators extends parametersAndOperatorsMain {
+    public int calc(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+}
---
+++ src/parametersAndOperatorsMain.java
@@ -0,0 +1,6 @@
+public class parametersAndOperatorsMain {
+    public static void main(String[] args) {
+        ParametersAndOperators po = new ParametersAndOperators();
+        System.out.println(po.calc(2, 3));
+    }
+}
